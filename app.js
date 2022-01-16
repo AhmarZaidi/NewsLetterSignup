36426a1ca1413692bb1221fb5c77d2efd601b87a
Ahmar <ahmarzaidi07@gmail.com>
2022-01-16 19:10:15 +0530

    add subscribe function and mailchimp connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const https = require("https");
+require('dotenv').config();
 
 const app = express(); // express module
 var port = 3000; // localhost:3000
@@ -14,15 +15,58 @@ app.get("/", function(req, res){ // At localhost:{PORT}/
 
 app.post("/", function(req, res){  // At localhost:{PORT}/
     
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName, lastName, email);
-    // res.send();
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    // console.log(firstName, lastName, email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+ 
+    const url = "https://us" + process.env.REGION + ".api.mailchimp.com/3.0/lists/" + process.env.LIST_ID; 
+    console.log(url);
+    
+    const options = {
+        method: "POST",
+        auth: "chimp:" + process.env.API_KEY
+    }
+    console.log(options.auth);
+    
+    const request = https.request(url, options, function(response){
+        
+        // console.log(response.statusCode);
+        if(response.statusCode === 200)
+            res.sendFile(__dirname + "/success.html")
+        else
+            res.sendFile(__dirname + "/failure.html")
+
+        response.on("data", function(data){
+            // console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
 
 
-app.listen(port, function(){ 
+app.listen(process.env.PORT || port, function(){ 
     console.log("Server started on port " + port);
     console.log("Server Running...");
 })
